68e50f81283be9b7a1ffc8fc817b8cdedde5d03f
Jose A Dianes <jadianes@gmail.com>
2014-09-21 14:55:00 +0100

    new S4 object representation for project summary

---
+++ R/project.summary.R
@@ -0,0 +1,57 @@
+setClass(
+    "ProjectSummary", 
+    representation(
+        accession = "character", 
+        title = "character",
+        projectDescription = "character",
+        publicationDate = "POSIXct",
+        numAssays = "numeric",
+        species = "vector",
+        tissues = "vector",
+        ptmNames = "vector",
+        instrumentNames = "vector",
+        projectTags = "vector",
+        submissionType = "character"
+    ),
+    prototype(
+        accession = "Not available", 
+        title = "Not available",
+        projectDescription = "Not available",
+        publicationDate = Sys.time(),
+        numAssays = 0,
+        species = c("Not available"),
+        tissues = c("Not available"),
+        ptmNames = c("Not available"),
+        instrumentNames = c("Not available"),
+        projectTags = c("Not available"),
+        submissionType = "Not available"
+    )
+)
+
+as.data.frame.ProjectSummary <-
+    function(x, row.names=NULL, optional=FALSE, ...)
+{
+    # set row names if provided
+    if (is.null(row.names))
+        row.names <- x@accession
+    # create the data frame just with the accession column
+    value <- list(x@accession)
+    attr(value, "row.names") <- row.names
+    class(value) <- "data.frame"
+    names(value) <- c("accession")
+    # add the rest of the columns
+    value$title <- x@title
+    value$project.description <- x@projectDescription
+    value$publication.date <- x@publicationDate
+    value$num.assays <- x@numAssays
+    value$species <- paste(x@species, collapse=" || ")
+    value$tissues <- paste(x@tissues, collapse=" || ")
+    value$ptm.names <- paste(x@ptmNames, collapse=" || ")
+    value$instrument.names <- paste(x@instrumentNames, collapse=" || ")
+    value$project.tags <- paste(x@projectTags, collapse=" || ")
+    value$submissionType <- x@submissionType
+    
+    return(value)
+}
+
+format.ProjectSummary <- function(x, ...) paste0(x@accession, ", ", x@title)
